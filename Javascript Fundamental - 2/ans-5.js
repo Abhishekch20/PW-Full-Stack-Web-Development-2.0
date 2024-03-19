27df48281716abd5c561db8d73e7573670dc02cf
Abhishek Chiluveru <142038654+Abhishekch20@users.noreply.github.com>
2024-03-19 21:48:47 +0530

    Delete Javascript Fundamental - 2 directory

--- Javascript Fundamental - 2/ans-5.js
+++
@@ -1,5 +0,0 @@
-// Array of guest name as strings
-const guest = ["Anurag","Mithiun","Alka","Prabhir","Shivam","Farman"]
-
-// Array Method
-console.log(guest.join(', '))
--- Javascript Fundamental - 2/ans-7.js
+++
@@ -1,7 +0,0 @@
-
-function generateOTP(){
-    let num=Math.random()*9000
-    num=Math.floor(num) + 1000
-    return num
-}
-console.log(`Here is your otp: ${generateOTP()}`);
