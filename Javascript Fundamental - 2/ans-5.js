0f0abc500833fa917528d0f454be07ab33033aa6
Abhishek Chiluveru <142038654+Abhishekch20@users.noreply.github.com>
2024-03-19 21:50:31 +0530

    Add files via upload

---
+++ Javascript Fundamental - 2/ans-5.js
@@ -0,0 +1,5 @@
+// Array of guest name as strings
+const guest = ["Anurag","Mithiun","Alka","Prabhir","Shivam","Farman"]
+
+// Array Method
+console.log(guest.join(', '))
---
+++ Javascript Fundamental - 2/ans-6.js
@@ -0,0 +1,10 @@
+const productDetails = {
+    name:"Apple 2020 MacBook Air Laptop",
+    price: 82000,
+    color:"Grey",
+    hardDisk: "256 GB"
+}
+
+console.log('Below are the products details.')
+
+console.log(` name : ${productDetails.name} \n price : ${productDetails.price} \n color : ${productDetails.price} \n hardDisk : ${productDetails.hardDisk}`)
